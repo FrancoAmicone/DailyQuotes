2f6d1321880bfcc5edd93ce636fa7f0621d1ede1
FrancoAmicone <francoamicone1@gmail.com>
2024-06-14 17:03:57 -0300

    update(biographyBorges)

--- components/DailyQuoteScreen.jsx
+++ components/DailyQuoteScreen.jsx
@@ -1,45 +1,59 @@
-// components/DailyQuoteScreen.js
 import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity  } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import quotesData from '../data/data.json';
 import { Ionicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
 
-const DailyQuoteScreen = ({navigation}) => {
+const DailyQuoteScreen = ({ navigation }) => {
   const [fontsLoaded] = useFonts({
     Neue: require("../assets/fonts/NeueMontreal-Medium.otf"),
     Shibui: require("../assets/fonts/Shibui.ttf"),
   });
-  
+
   const [quote, setQuote] = useState(null);
 
   useEffect(() => {
     const loadSelectedAuthor = async () => {
-      const storedAuthor = await AsyncStorage.getItem('selectedAuthor');
-      if (storedAuthor) {
-        const author = quotesData.authors.find(author => author.name === storedAuthor);
-        if (author) {
-          const randomQuote = author.quotes[Math.floor(Math.random() * author.quotes.length)];
-          setQuote({ quote: randomQuote, author: author.name, image: author.image, description: author.description });
+      try {
+        const storedAuthor = await AsyncStorage.getItem('selectedAuthor');
+        if (storedAuthor) {
+          const author = quotesData.authors.find(author => author.name === storedAuthor);
+          if (author) {
+            const randomQuote = author.quotes[Math.floor(Math.random() * author.quotes.length)];
+            setQuote({ 
+              quote: randomQuote, 
+              author: author.name, 
+              image: author.image, 
+              description: author.description, 
+              biography: author.biography 
+            });
+          }
         }
+      } catch (error) {
+        console.error('Error loading author data:', error);
       }
     };
     loadSelectedAuthor();
   }, []);
 
   if (!fontsLoaded || !quote) {
-    return null;
+    return null;  // Retornamos null mientras se cargan las fuentes o si aún no se ha cargado la cita
   }
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.title}>Daily Quote</Text>
+      <Text style={styles.title}>Daily Quotes</Text>
       <Image source={{ uri: quote.image }} style={styles.authorImage} />
       <Text style={styles.author}>{quote.author}</Text>
       <Text style={styles.description}>{quote.description}</Text>
 
-      <TouchableOpacity  onPress={() => navigation.navigate('Home')} style={styles.card}>
+      {/* Mostrar la biografía del autor */}
+      <Text style={styles.author}>Biografia</Text>
+      <Text style={styles.description}>{quote.biography}</Text>
+
+      {/* Botón para navegar de regreso a la pantalla de inicio */}
+      <TouchableOpacity onPress={() => navigation.navigate('Home')} style={styles.card}>
         <Ionicons name="home-outline" size={24} color="black" style={styles.icon} />
         <Text style={styles.cardText}>Back to Home</Text>
       </TouchableOpacity>
@@ -55,27 +69,23 @@ const styles = StyleSheet.create({
     padding: 20,
   },
   title: {
-    fontSize: 36,
+    fontSize: 50,
     marginBottom: 20,
     fontFamily: "Shibui",
+    textAlign: 'center',
+    top: '1%',
   },
   authorImage: {
     width: 300,
-    height: 300,
+    height: 400,
     borderRadius: 150,
-    marginBottom: 20,
+    margin:50,
   },
   author: {
+    margin:20,
     fontSize: 30,
     fontFamily: "Neue",
     marginBottom: 10,
-  },
-  quote: {
-    fontSize: 18,
-    fontFamily: "Neue",
-    textAlign: 'center',
-    marginBottom: 20,
-    paddingHorizontal: 20,
   },
   description: {
     fontSize: 18,
@@ -84,6 +94,7 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
   },
   card: {
+
     flexDirection: 'row',
     alignItems: 'center',
     backgroundColor: '#f0f0f0',
@@ -91,15 +102,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     elevation: 3,
     marginTop: 20,
-    fontFamily:"Neue",
+    fontFamily: "Neue",
   },
   icon: {
-  marginRight: 10,
-},
-cardText: {
-  fontSize: 16,
-  fontFamily:"Neue",
-},
+    marginRight: 10,
+  },
+  cardText: {
+    fontSize: 16,
+    fontFamily: "Neue",
+  },
 });
 
 export default DailyQuoteScreen;
